ceec72fb4fd0fb9267931160d03ebe5d1b67985a
varun <varun@varuns-MacBook-Air.local>
2026-02-25 16:49:47 +0530

    episode-02&03

--- App.js
+++ App.js
@@ -1,20 +1,61 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div",
-        { id: "parent" },
-    [
-          React.createElement("div", { id: "child" }, [
-              React.createElement("h1", {}, "Im an h1 tag"),
-              React.createElement("h2", {}, "Im an h2 tag")
-       ]),
-          React.createElement("div", { id: "child2" }, [
-              React.createElement("h1", {}, "Im an h1 tag"),
-              React.createElement("h2", {}, "Im an h2 tag")
-       ])
-    ]
+
+/**
+ * Header
+ *  - Logo
+ * - Nav Items
+ * Body
+ * - Search Bar
+ * - Restaurant List
+ *  - Restaurant Card
+ *    - Image
+ *    - Name
+ *   - Rating
+ *    - Cusines
+ * Footer
+ *  - copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ */
+
+
+// React.createElement ==> ReactElement-JS Object => HTMLElement(render)
+
+const elem =<span>React Element</span>
+
+const title =  (
+  <h1 className="head" tabIndex="1">
+    {elem}
+    Namaste React from using JSX
+  </h1>
+);
+// React Functional Component - A normal JS function which returns JSX
+
+// componet composition - using a component inside another component
+
+const HeadingComponent = () => (
+  <div id="container">
+    {title}
+    <h1 className="heading" tabIndex="1">
+      Namaste React from using Functional Component
+    </h1>
+  </div>
 );
 
+// JSX - is not HTML but it looks like HTML (transpiled before it reaches the JS) - PARCEL - BABEL
 
-const heading = React.createElement("h1", { id: "heading", xyz: "test" }, "Hello world from React");
-console.log(heading);
+//JSX ==> React.createElement ==> ReactElement-JS Object => HTMLElement(render)
+
+const jsxHeading = (
+  <h1 className="head" tabIndex="1">
+    Hello World from JSX
+  </h1>
+);
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<HeadingComponent />);
